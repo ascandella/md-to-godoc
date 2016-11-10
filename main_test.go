8ea74569c35191f76955a19156c75c97a025a77a
Aiden Scandella <git@sca.ndella.com>
2016-11-09 17:45:15 -0800

    Begin testing main package

---
+++ main_test.go
@@ -0,0 +1,87 @@
+// Copyright 2016 Aiden Scandella
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReader_Stdin(t *testing.T) {
+	defer overrideBool(stdin, true)()
+
+	assert.Equal(t, os.Stdin, reader())
+}
+
+func TestReader_BadFile(t *testing.T) {
+	defer overrideString(inFile, "non-existent")()
+
+	assert.Panics(t, func() {
+		reader()
+	})
+}
+
+func TestReader_GoodFile(t *testing.T) {
+	r := reader()
+
+	assert.NotEqual(t, os.Stdin, r)
+}
+
+func TestWriter_Stdout(t *testing.T) {
+	defer overrideBool(stdout, true)()
+
+	assert.Equal(t, os.Stdout, writer())
+}
+
+func TestPackageName_Overridden(t *testing.T) {
+	defer overrideString(pkgName, "seattle")()
+
+	assert.Equal(t, "seattle", packageName())
+}
+
+func TestPackageName_Inferred(t *testing.T) {
+	assert.Equal(t, "main", packageName())
+}
+
+func TestPackageName_BadDirPanics(t *testing.T) {
+	defer overrideString(inFile, "/foobar")()
+
+	assert.Panics(t, func() {
+		packageName()
+	})
+}
+
+func TestPackageName_Relative(t *testing.T) {
+	defer overrideString(inFile, "render/README.md")()
+
+	assert.Equal(t, "render", packageName())
+}
+
+func overrideBool(target *bool, val bool) func() {
+	old := *target
+	*target = val
+	return func() {
+		*target = old
+	}
+}
+
+func overrideString(target *string, val string) func() {
+	old := *target
+	*target = val
+	return func() {
+		*target = old
+	}
+}
